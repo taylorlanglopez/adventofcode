7fd269f232d35504cd263006c55a0feaa27f28be
Taylor Lopez <tllopez@protonmail.com>
2021-12-09 11:54:03 -0800

    Trying to get a utils module

---
+++ Advent_of_Code_2021/utils/go.mod
@@ -0,0 +1,3 @@
+module github.com/taylorlanglopez/adventofcode/Advent_of_Code_2021/utils
+
+go 1.17
---
+++ Advent_of_Code_2021/utils/utils.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+func checkError(e error) {
+	if e != nil {
+		fmt.Println("Error ->", e)
+		log.Fatal(e)
+	}
+}
+
+func TotalProgramTime() func() {
+	start := time.Now()
+	return func() {
+		end := time.Now()
+		fmt.Printf("Total Time: %d ms\n",
+			end.Sub(start).Milliseconds())
+	}
+}
