1b73cbac7657ac882be1354a58e0341f42e352e6
Taylor Lopez <tllopez@protonmail.com>
2021-12-10 19:38:49 -0800

    Day8 part1

--- Advent_of_Code_2021/Day8/SevenSegment.go
+++ Advent_of_Code_2021/Day8/SevenSegment.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/taylorlanglopez/adventofcode/Advent_of_Code_2021/utils"
 )
 
 func SevenSegment(fileName string) {
 	defer utils.TotalProgramTime()()
-	file, err := ioutil.ReadFile(fileName)
-
+	file, err := os.Open(fileName)
 	utils.CheckError(err)
 
-	fmt.Println(file)
+	scanner := bufio.NewScanner(file)
 
-	fmt.Println("Hello World!")
+	lastFourCodeStrings := make([]string, 0)
+
+	// # => # of segments
+	// 1 == 2
+	// 4 == 4
+	// 7 == 3
+	// 8 == 7
+
+	for scanner.Scan() {
+		for _, i := range strings.Fields(strings.Split(scanner.Text(), "|")[1]) {
+			lastFourCodeStrings = append(lastFourCodeStrings, i)
+		}
+	}
+
+	countKeys := 0
+	for _, v := range lastFourCodeStrings {
+		if len(v) == 2 || len(v) == 4 || len(v) == 3 || len(v) == 7 {
+			countKeys++
+		}
+	}
+
+	fmt.Println(countKeys)
 }
--- Advent_of_Code_2021/Day8/go.mod
+++ Advent_of_Code_2021/Day8/go.mod
@@ -3,3 +3,4 @@ module github.com/taylorlanglopez/adventofcode/Advent_of_Code_2021/Day8
 go 1.17
 
 require github.com/taylorlanglopez/adventofcode/Advent_of_Code_2021/utils v0.0.0-20211209203056-48a081debe9c
+replace github.com/taylorlanglopez/adventofcode/Advent_of_Code_2021/utils => ../utils
